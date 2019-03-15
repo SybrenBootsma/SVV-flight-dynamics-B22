73312c4ee4cc991f7681f73580ea2a622a848d7d
Max Knoop <maxknoop200@gmail.com>
2019-03-15 12:03:17 +0100

    Update Ref_data.py

--- Ref_data.py
+++ Ref_data.py
@@ -11,10 +11,27 @@ tas = np.genfromtxt("matlab/Dadc1_tas.csv", dtype="float")
 
 
 for i in range(len(time)):
+    
     pitch_rate[i] = np.deg2rad(pitch_rate[i])
     delta_e[i] = np.deg2rad(delta_e[i])
     alpha[i] = np.deg2rad(alpha[i])
     pitch[i] = np.deg2rad(pitch[i])
+
+def Pheugoid_init():
+    
+    for i in range(len(time)):
+        if time[i] == 3223.:
+            begin_p = i
+        if time[i] == 3228.:
+            end_p = i
+    
+    hp0_p = np.mean(hp[begin_p:end_p]*0.3048)
+    tas_p = np.mean(tas[begin_p:end_p])
+    alpha0_p = np.mean(alpha[begin_p:end_p])
+    pitch0_p = np.mean(pitch[begin_p:end_p])
+    
+    return hp0_p, tas_p, alpha0_p, pitch0_p
+
 
 def Pheugoid():
       
@@ -24,21 +41,15 @@ def Pheugoid():
             begin_p = i
         if time[i] == 3457.:
             end_p = i
-    
     #pheugoid lists
     time_p = time[begin_p:end_p]
     pitch_rate_p = pitch_rate[begin_p:end_p]
     delta_e_p = delta_e[begin_p:end_p]
     alpha_p = alpha[begin_p:end_p]
     pitch_p = pitch[begin_p:end_p]
-    jij = tas[begin_p:end_p]
+    u_p = tas[begin_p:end_p]
     
-    hp0_p = hp[begin_p]*0.3048 
-    tas_p = tas[begin_p]
-    alpha0_p = alpha[begin_p]
-    pitch0_p = pitch[begin_p]
-    
-    return time_p, pitch_rate_p, delta_e_p, alpha_p, pitch_p, jij, hp0_p, tas_p, alpha0_p, pitch0_p
+    return time_p, pitch_rate_p, delta_e_p, alpha_p, pitch_p, u_p
 
 
 def Short_period():
@@ -59,11 +70,14 @@ def Short_period():
     return time_s, pitch_rate_s, delta_e_s, alpha_s
 
 
+time_p, pitch_rate_p, delta_e_p, alpha_p, pitch_p, u_p = Pheugoid()
+
+
 #plt.subplot(121)
-#plt.plot(time_p,pitch_rate_p, label = 'pitch rate')
-#plt.plot(time_p,delta_e_p, label = 'delta e')
+plt.plot(time_p,pitch_rate_p, label = 'pitch rate')
+plt.plot(time_p,delta_e_p, label = 'delta e')
 #plt.plot(time_p,alpha_p, label = 'alpha')
-#plt.legend()
+plt.legend()
 #
 #plt.subplot(122)
 #plt.plot(time_s,pitch_rate_s, label = 'pitch rate')
